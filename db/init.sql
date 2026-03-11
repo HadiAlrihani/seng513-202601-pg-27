33e3295797494f3801bb12979817c53e6cf74975
Hadi Alrihani <abdulhadi.alrihani@ucalgary.ca>
2026-03-10 23:58:33 -0600

    feat(database): add initial database schema

---
+++ db/init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    email TEXT UNIQUE,
+    user_password TEXT NOT NULL
+);
+
+CREATE TABLE bookclubs (
+    id SERIAL PRIMARY KEY,
+    club_name TEXT NOT NULL,
+    number_members INTEGER NOT NULL,
+    book_id INTEGER REFERENCES books(isbn)
+);
+
+CREATE TABLE bookclub_members(
+    user_id INTEGER REFERENCES users(id),
+    club_id INTEGER REFERENCES bookclubs(id),
+    PRIMARY KEY (user_id, club_id)
+);
+
+CREATE TABLE books(
+    isbn TEXT PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL
+);
