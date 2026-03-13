87695080baeb03e36720d2a20f2b1fd8dcb9e73f
Hadi Alrihani <abdulhadi.alrihani@ucalgary.ca>
2026-03-12 19:10:09 -0600

    fix(database): reorder init.sql table creation

--- db/init.sql
+++ db/init.sql
@@ -5,21 +5,21 @@ CREATE TABLE users (
     user_password TEXT NOT NULL
 );
 
+CREATE TABLE books(
+    isbn TEXT PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL
+);
+
 CREATE TABLE bookclubs (
     id SERIAL PRIMARY KEY,
     club_name TEXT NOT NULL,
     number_members INTEGER NOT NULL,
-    book_id INTEGER REFERENCES books(isbn)
+    book_id TEXT REFERENCES books(isbn)
 );
 
 CREATE TABLE bookclub_members(
     user_id INTEGER REFERENCES users(id),
     club_id INTEGER REFERENCES bookclubs(id),
     PRIMARY KEY (user_id, club_id)
-);
-
-CREATE TABLE books(
-    isbn TEXT PRIMARY KEY NOT NULL,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL
 );
