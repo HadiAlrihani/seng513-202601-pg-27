f8760bfd87322c7835c28dbb6127f594cd548b23
Hadi Alrihani <abdulhadi.alrihani@ucalgary.ca>
2026-04-05 21:46:40 -0600

    feat(auth)-login and create account working -login and create account communicates with backend -final styling changes

--- client/src/pages/CreateAccount.jsx
+++ client/src/pages/CreateAccount.jsx
@@ -8,27 +8,26 @@ export default function CreateAccount() {
 
     const formRef = useRef();
 
-    const handleRegistration = async () => {
+    const handleRegistration = async (e) => {
+        e.preventDefault()
         const formData = new FormData(formRef.current);
 
         const email = formData.get("email");
         const username = formData.get("username");
-        const password = formData.get("password");
+        const user_password = formData.get("password");
 
         try {
             const response = await fetch ("http://localhost:5000/users/register", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ email, username, password})
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, username, user_password })
             });
 
             const data = await response.json();
 
             if (response.ok) {
                 console.log("Account created:", data),
-                navigate("/home")
+                navigate("/select-genres")
             }
             else {
                 console.log("Creation failed: ", data.message)
@@ -41,30 +40,31 @@ export default function CreateAccount() {
 
     return (
         <div className="h-full flex flex-col justify-evenly items-center py-[5vh] px-[10vh] md-short:py-[7vh]">
-            <div className="flex md-computer:w-[60vw] max-h-[25vh]">
-                <img className="hidden md-computer:block" src={logo} />
-                <h1 className="flex font-italiana text-6xl md:text-8xl md-short:text-5xl text-center">
+            <div className="flex md-computer:w-[60vw] max-h-[25vh] items-center justify-center">
+                <img className="hidden md-computer:block max-h-[25vh]" src={logo} />
+                <h1 className="flex font-italiana text-6xl md:text-7xl md-short:text-5xl text-center">
                     Wormly Connected
                 </h1>
             </div>
-            <form onSubmit={handleRegistration} 
+            <form ref={formRef} 
+            onSubmit={handleRegistration} 
             className="flex flex-col w-[75vw] md:w-[60vw] gap-8 md-short:gap-4 text-lg md:text-2xl">
                 <input
                     type="text"
                     placeholder="Email"
                     name="email"
-                    className="h-15 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
+                    className="h-12 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
                 <input
                     type="text"
                     placeholder="Username"
                     name="username"
-                    className="h-15 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
+                    className="h-12 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
                 <input
                     type="password"
                     placeholder="Password"
                     name="password"
-                    className="h-15 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4" />
-                <button onClick={() => navigate("/select-genres")}
+                    className="h-12 md:h-20 bg-[#d9d9d9] placeholder-black/65 p-4" />
+                <button
                     type="submit"
                     className="bg-[#D3F0D3]/80 w-fit self-center md:px-20 text-lg md:text-3xl p-3 rounded-xl">
                         Create Account
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -10,19 +10,18 @@ export default function Login() {
     //Reference for the login form
     const formRef = useRef();
 
-    const handleLogin = async () => {
+    const handleLogin = async (e) => {
+        e.preventDefault()
         const formData = new FormData(formRef.current);
 
         const username_or_email = formData.get("username-or-email");
-        const password = formData.get("password");
+        const user_password = formData.get("password");
 
         try {
             const response = await fetch("http://localhost:5000/users/login", {
                 method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ username_or_email, password })
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username_or_email, user_password })
             });
 
             const data = await response.json();
@@ -42,39 +41,37 @@ export default function Login() {
     }
 
     return (
-        <div className="h-full flex flex-col items-center py-[20vh] md:py-[20vh] px-[10vh]">
-            <div className="flex max-h-[25vh] md-computer:max-w-[60vw]">
-                <img className="hidden md-computer:block" src={logo} />
-                <h1 className="flex-1 font-italiana text-6xl md:text-8xl text-center mb-24 md-short:mb-8">
+        <div className="h-full flex flex-col justify-around items-center py-[20vh] md:py-[20vh] md-computer:py-[5vh] px-[10vh]">
+            <div className="flex max-h-[25vh] md-computer:max-w-[60vw]  items-center justify-center">
+                <img className="hidden md-computer:block max-h-[25vh]" src={logo} />
+                <h1 className="flex-1 font-italiana text-6xl md:text-7xl text-center">
                     Wormly Connected
                 </h1>
             </div>
             <form ref={formRef}
             onSubmit={handleLogin}
-            className="flex-1 flex flex-col w-[75vw] md:w-[60vw] gap-8 text-lg md:text-xl md-computer:text-2xl mb-4 md-short:mb-4">
+            className="flex flex-col justify-around w-[75vw] md:w-[60vw] gap-8 text-lg md:text-xl md-computer:text-2xl mb-4 md-short:mb-4">
                 <input
                     type="text"
                     placeholder="Username/Email"
                     name="username-or-email"
-                    className="h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
+                    className="h-16 bg-[#d9d9d9] placeholder-black/65 p-4"/>
                 <input
                     type="password"
                     placeholder="Password"
                     name="password"
-                    className="h-20 bg-[#d9d9d9] placeholder-black/65 p-4"/>
+                    className="h-16 bg-[#d9d9d9] placeholder-black/65 p-4"/>
                 <button 
                     type="submit" 
-                    className="self-bottom text-center text-2xl md:text-3xl md-computer:text-4xl">
+                    className="self-bottom w-fit self-center text-center text-2xl md:text-3xl md-computer:text-4xl">
                         Login
                 </button>
-            </form>
-            <div className="flex-1 flex items-start text-2xl md:text-3xl md-computer:gap-10 md-short:gap-3 md-computer:text-4xl">
                 <button 
                     onClick={() => navigate("/create-account")} 
-                    className="text-center text-black/70">
+                    className="self-center text-center w-fit text-black/70">
                         Create account
                 </button>
-            </div>
+            </form>
         </div>
     )
 }
