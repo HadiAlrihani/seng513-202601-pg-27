6e5551815228622b14fda77eb3372471ebea86c1
Hadi Alrihani <abdulhadi.alrihani@ucalgary.ca>
2026-04-08 13:27:39 -0600

    feat(ui)-Complete responsive sidebar functionality -Add correct icons for buttons -Buttons unresponsive until pages are completed

--- client/src/components/HomeSidebar.jsx
+++ client/src/components/HomeSidebar.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
 import bookshelf from "../assets/bookshelf.png";
+import find_icon from "../assets/find.png";
+import discussion from "../assets/discussion.png";
 
 export default function HomeSidebar() {
     const navigate = useNavigate();
@@ -9,29 +11,29 @@ export default function HomeSidebar() {
         <div className="flex justify-between items-start
              md-computer:flex-col md-computer:h-[85vh] md-computer:items-center md-computer:w-[15vw] md-computer:bg-[#D3F0D3]/35">
             <div className="flex flex-col flex-1 items-center md-computer:justify-evenly">
-                <button onClick={() => navigate('/friends-shelf')} 
-                className="flex items-center justify-center w-24 h-24 box-border rounded-full bg-[#D3F0D3] flex justify-center items-center
-                md-computer:h-32 md-computer:w-32">
+                <button 
+                className="flex items-center justify-center w-[24vw] h-[24vw] box-border rounded-full bg-[#D3F0D3] flex justify-center items-center
+                md-computer:h-[10vw] md-computer:w-[10vw]">
                     <img src={bookshelf} 
-                    className="h-14 w-14 md-computer:h-20 md-computer:w-20 object-contain" />
+                    className="h-[14vw] w-[14vw] md-computer:h-[6vw] md-computer:w-[6vw] object-contain" />
                 </button>
                 <h1 className="font-inter text-center md-computer:text-lg">Friend's Shelf</h1>
             </div>
             <div className="flex flex-col flex-1 items-center md-computer:justify-evenly">
                 <button onClick={() => navigate('/clubs')} 
-                className="flex items-center justify-center w-24 h-24 box-border rounded-full bg-[#D3F0D3]
-                md-computer:h-32 md-computer:w-32">
-                    <img src={bookshelf} 
-                    className="h-14 w-14 md:h-20 md:w-20 md-computer:h-20 md-computer:w-20 object-contain" />
+                className="flex items-center justify-center w-[24vw] h-[24vw] box-border rounded-full bg-[#D3F0D3]
+                md-computer:h-[10vw] md-computer:w-[10vw]">
+                    <img src={find_icon} 
+                    className="h-[14vw] w-[14vw] md-computer:h-[6vw] md-computer:w-[6vw] object-contain" />
                 </button>
                 <h1 className="font-inter text-center md-computer:text-lg">Find a Club</h1>
             </div>
             <div className="flex flex-col flex-1 items-center md-computer:justify-evenly">
                 <button 
-                className="flex items-center justify-center w-24 h-24 box-border rounded-full bg-[#D3F0D3]
-                md-computer:h-32 md-computer:w-32">
-                    <img src={bookshelf} 
-                    className="h-14 w-14 md-computer:h-20 md-computer:w-20 object-contain" />
+                className="flex items-center justify-center w-[24vw] h-[24vw] box-border rounded-full bg-[#D3F0D3]
+                md-computer:h-[10vw] md-computer:w-[10vw]">
+                    <img src={discussion} 
+                    className="h-[14vw] w-[14vw] md-computer:h-[6vw] md-computer:w-[6vw] object-contain" />
                 </button>
                 <h1 className="font-inter text-center md-computer:text-lg">Recent Discussion</h1>
             </div>
