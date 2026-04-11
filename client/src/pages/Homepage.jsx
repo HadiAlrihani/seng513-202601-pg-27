f8b58dcd13ecf5fff822daf9e0ec2ac1c700c43c
Hadi Alrihani <abdulhadi.alrihani@ucalgary.ca>
2026-04-10 18:37:37 -0600

    fix(routing)-Fix recent club navigation - Update Reading Progress now navigates to last viewed club

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -17,6 +17,31 @@ export default function Homepage() {
     }
   }, [username, navigate]);
 
+    const storedUserId =
+        localStorage.getItem("userId") || localStorage.getItem("wormly_id");
+    const userId = storedUserId ? Number(storedUserId) : null;
+
+    const handleRecentClub = async () => {
+
+        const response = await fetch(`http://localhost:5000/bookclubs/get-recent-club?userId=${userId}`, {
+            method: "GET",
+            headers: { "Content-Type": "application/json", },
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            console.log({ error: response})
+        }
+        else {
+            const clubId = data.club_id;
+            if (!clubId) {
+                return;
+            }
+            navigate(`/clubs/${clubId}/discussion`);
+        }
+    };
+
     return  (
         <>
         <Navbar />
@@ -33,7 +58,7 @@ export default function Homepage() {
                 </h1>
                 <div className="flex justify-evenly">
                     <div className="flex flex-1 flex-col items-center">
-                        <button onClick={() => navigate('/friends-shelf')} 
+                        <button onClick={handleRecentClub} 
                         className="flex items-center justify-center w-[44vw] h-[44vw] box-border rounded-full bg-[#D3F0D3] flex justify-center items-center
                         md-computer:h-[20vw] md-computer:w-[20vw]">
                             <img src={book_with_worm} 
